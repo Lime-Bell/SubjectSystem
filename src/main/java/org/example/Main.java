97f7431711c6a91d0c834f86a3edbb22a593c3df
lime1124 <weichen891124@gmail.com>
2022-12-05 01:07:37 +0800

    Annotation GUI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,13 @@ import java.util.*;
 import javax.swing.JFrame;
 
 
-//public class Main {
-//    public static void main(String[] args){
-//        // Initialized DB
-//        UserDB.Initialized();
-//        CourseDB.Initialized();
+public class Main {
+    public static void main(String[] args){
+        // Initialized DB
+        UserDB.Initialized();
+        CourseDB.Initialized();
 
-        //Start from Login Interface
-////        AllInterface.ShowLoginInterface();
-//    }
-//}
+//        Start from Login Interface
+//        AllInterface.ShowLoginInterface();
+    }
+}
